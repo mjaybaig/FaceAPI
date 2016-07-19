28f9074ab9165cd0ba041382dd2888b0d2d8cbf5
jameel.baig <jameel.baig@JAMEELDEVPC>
2016-07-19 18:02:46 +0500

    Add project files.

---
+++ FaceVerification/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace FaceVerification
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 303);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(500, 276);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(508, 343);
+            this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(159, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select First Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Location = new System.Drawing.Point(12, 361);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(508, 313);
+            this.panel2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(159, 285);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Select Second Image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.textBox1);
+            this.panel3.Controls.Add(this.btnCompare);
+            this.panel3.Location = new System.Drawing.Point(556, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(411, 658);
+            this.panel3.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 103);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(351, 185);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Location = new System.Drawing.Point(75, 58);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(254, 23);
+            this.btnCompare.TabIndex = 0;
+            this.btnCompare.Text = "Compare Images";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 682);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Face Verification";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Diagnostics.EventLog eventLog1;
+    }
+}
+
---
+++ FaceVerification/Form1.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using Newtonsoft.Json.Linq;
+
+namespace FaceVerification
+{
+    public partial class Form1 : Form
+    {
+        IFaceServiceClient faceServiceClient;
+        PleaseWaitForm pleaseWait;
+        public Form1()
+        {
+            InitializeComponent();
+            faceServiceClient = new FaceServiceClient("16306c6a4b53406da6eca3fb9e42dedb");
+            pleaseWait = new PleaseWaitForm();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+           
+            this.pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+           
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+            
+            this.pictureBox2.Image = Image.FromFile(openFileDialog2.FileName);
+        }
+
+        private async void btnCompare_Click(object sender, EventArgs e)
+        {
+            //            Microsoft.ProjectOxford.Face.FaceServiceClient f = new Microsoft.ProjectOxford.Face.FaceServiceClient("16306c6a4b53406da6eca3fb9e42dedb");\
+           
+            textBox1.Text = "";
+            Face[] faces = new Face[2];
+
+
+            this.UseWaitCursor = true;
+            // Display form modelessly
+           // pleaseWait.Show();
+
+            //  ALlow main UI thread to properly display please wait form.
+            //Application.DoEvents();
+
+            // Show or load the main form.
+            //mainForm.ShowDialog();
+
+            faces[0] = await UploadAndDetectFaces(openFileDialog1.FileName);
+            faces[1] = await UploadAndDetectFaces(openFileDialog2.FileName);
+
+            //this.textBox1.Text = faces[0].FaceId.ToString();
+
+            VerifyFace(faces);
+
+            
+
+        }
+
+        private async Task<Face> UploadAndDetectFaces(string imageFilePath)
+        {
+            try
+            {
+                using (Stream imageFileStream = File.OpenRead(imageFilePath))
+                {
+                    //faceServiceClient
+                    Face[] faces = await faceServiceClient.DetectAsync(imageFileStream);
+                    return faces[0];
+                   // var faceRects = faces.Select(face => face.FaceRectangle);
+                   // return faceRects.ToArray();
+                }
+            }
+            catch (Exception)
+            {
+                return new Face();
+            }
+        }
+
+
+        async void VerifyFace(Face[] faces)
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "16306c6a4b53406da6eca3fb9e42dedb");
+            
+            var uri = "https://api.projectoxford.ai/face/v1.0/verify?" + queryString;
+
+            HttpResponseMessage response;
+
+            string body = "{\"faceId1\":\""+ faces[0].FaceId + "\",\"faceId2\":\""+ faces[1].FaceId + "\"}";
+
+            //string body = "{\"faceId1\":\"aba4d170-475d-4a6b-83f2-989693d2f9c1\",\"faceId2\":\"aba4d170-475d-4a6b-83f2-989693d2f9c1\"}";
+
+
+             /*// Request body
+             byte[] byteData = Encoding.UTF8.GetBytes(body);
+
+
+            //Newtonsoft.Json.Linq.JObject obj = new Newtonsoft.Json.Linq.JObject();
+
+            
+             using (var content = new ByteArrayContent(byteData))
+             {
+                 content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                 response = await client.PostAsync(uri, content);
+             }*/
+            HttpContent theContent = new StringContent(body, System.Text.Encoding.UTF8, "application/json");
+           
+            //HttpContent cont = theContent.
+            response = await client.PostAsync(uri, theContent);
+
+            String jsonString= await response.Content.ReadAsStringAsync();
+
+
+            JObject jasonObj = JObject.Parse(jsonString);
+            if(jasonObj!=null && jasonObj["isIdentical"].ToString().Equals("True"))
+            {
+                textBox1.Text = "The images are a match with " + (Decimal.Parse(jasonObj["confidence"].ToString())*100) +"% probability";
+            }
+            else if (jasonObj != null && jasonObj["isIdentical"].ToString().Equals("False"))
+            {
+                textBox1.Text = "The images do not match. Probability is " + (Decimal.Parse(jasonObj["confidence"].ToString()) * 100) + "%.";
+            }
+            else
+            {
+                textBox1.Text = jsonString;
+            }
+
+            //pleaseWait.Close();
+            this.UseWaitCursor = false;
+            //textBox1.Text = await response.Content.ReadAsStringAsync();
+
+
+            //response.Content.ToString();
+
+
+        }
+
+
+    }
+}
